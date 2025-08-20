50db4e79ab2d17d07b9863c5571c4be14bd44feb
dorothy <dorothy.luluya@gmail.com>
2025-08-20 15:02:36 +0800

    获取并渲染购物车列表

--- miniprogram/pages/cart/cart.js
+++ miniprogram/pages/cart/cart.js
@@ -1,7 +1,14 @@
 // pages/cart/component/cart.js
-Component({
-  // 组件的属性列表
-  properties: {},
+import { ComponentWithStore } from 'mobx-miniprogram-bindings'
+import { userStore } from '../../stores/userStore'
+import { reqCartList } from '../../api/cart'
+
+ComponentWithStore({
+  // 让页面和Store对象进行关联
+  storeBindings: {
+    store: userStore,
+    fields: ['token'],
+  },
 
   // 组件的初始数据
   data: {
@@ -10,5 +17,24 @@ Component({
   },
 
   // 组件的方法列表
-  methods: {}
+  methods: {
+    async showTipGoLogin() {
+      const { token } = this.data
+
+      if (!token) {
+        this.setData({
+          emptyDes: '您尚未登录，点击登录获取更多利益',
+          cartList: []
+        })
+        return
+      }
+
+      const res = await reqCartList()
+      console.log(res)
+    },
+
+    onShow() {
+      this.showTipGoLogin()
+    }
+  }
 })
